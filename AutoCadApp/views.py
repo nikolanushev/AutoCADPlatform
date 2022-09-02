e908c23e99bef00099434ed05bb43f7409da5dd6
Nikola Nushev <nikola_nusev@yahoo.com>
2022-09-02 20:26:06 +0200

    User updated

--- AutoCadApp/views.py
+++ AutoCadApp/views.py
@@ -2,29 +2,37 @@ import datetime
 
 from django.shortcuts import render
 from .models import Tutorial, AppUser
+from django.contrib.auth.models import User
 # Create your views here.
 
 
 def dashboard(request):
     tutorials = Tutorial.objects.all()[:3]
-    context = {"date": datetime.datetime.now(), "tutorials": tutorials}
+    user = AppUser.objects.filter(user=request.user).get()
+    context = {"date": datetime.datetime.now(), "tutorials": tutorials, 'user': user}
     return render(request, 'dashboard.html', context=context)
 
 
 def lectures(request):
     tutorials = Tutorial.objects.all()
-    #user = AppUser.objects.filter(username=request.user.username).all()
-    context = {'tutorials': tutorials}
+    user = AppUser.objects.filter(user=request.user).get()
+    context = {'tutorials': tutorials, 'user': user}
     return render(request, 'lectures.html', context)
 
 
 def exams(request):
-    return render(request, 'exams.html')
+    user = AppUser.objects.filter(user=request.user).get()
+    context = {'user': user}
+    return render(request, 'exams.html', context=context)
 
 
 def projects(request):
-    return render(request, 'projects.html')
+    user = AppUser.objects.filter(user=request.user).get()
+    context = {'user': user}
+    return render(request, 'projects.html', context=context)
 
 
 def profile(request):
-    return render(request, 'profile.html')
+    user = AppUser.objects.filter(user=request.user).get()
+    context = {'user': user}
+    return render(request, 'profile.html', context=context)
