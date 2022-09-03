a8d4e57563d8048fb028b2cfad93d6ca42a82a53
Nikola Nushev <nikola_nusev@yahoo.com>
2022-09-03 19:55:25 +0200

    added domain

--- AutoCadPlatform/settings.py
+++ AutoCadPlatform/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!y1fk)7(*a&1(mfwm*)b$r5zg3!&9wrj-la4bq@@6$2b^lvk1x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'autocadlearning.herokuapp.com']
 
 
 # Application definition
